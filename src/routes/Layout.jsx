866d0902fe0ba38e26e090dc8a8d8a6b77c3c214
Shakiqurrahman <rahmanshakiqur@gmail.com>
2024-05-26 23:37:50 +0600

    footer added

--- src/routes/Layout.jsx
+++ src/routes/Layout.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/shared/Header';
 import { Outlet, ScrollRestoration } from 'react-router-dom';
+import Footer from '../components/shared/Footer';
 
 const Layout = () => {
   return (
@@ -8,6 +9,7 @@ const Layout = () => {
       <Header />
       <ScrollRestoration />
       <Outlet />
+      <Footer />
     </>
   );
 };
